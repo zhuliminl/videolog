111935d8b2827db2d786d79519a4dfe6072414b3
zhuliminl <zhuliminl@gmail.com>
2020-05-10 14:44:58 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const querystring = require('querystring');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const csvWriter = createCsvWriter({
@@ -7,9 +8,11 @@ const csvWriter = createCsvWriter({
   header: [
     {id: 'clientType', title: '客户端'},
     {id: 'videoName', title: '视频名称'},
+    {id: 'videoHeight', title: '视频高度'},
+    {id: 'videoWidth', title: '视频宽度'},
+    {id: 'videoBitrate', title: '码率'},
     {id: 'videoSize', title: '视频大小'},
     {id: 'videoDuration', title: '视频时间'},
-    {id: 'videoBitrate', title: '码率'},
     {id: 'loadTime', title: '加载时间'},
     {id: 'testTime', title: '测试日期'},
   ]
@@ -26,11 +29,28 @@ router.get('/videolog', function(req, res, next) {
   const { query = {} } = req
 
   const testTime = new Date().toLocaleTimeString()
+  const {params = ''} = query
+  console.log('saul -------params', params)
+  const params_obj = querystring.parse(params)
+  const {
+    width = '',
+    height = '',
+    bitrate = '',
+  } = params_obj
+
   const data = [
-    {...query,
-    testTime
+    {
+    ...query,
+    testTime,
+    videoWidth: width,
+    videoHeight: height,
+    videoBitrate: bitrate,
+
     },
   ];
+
+  console.log('------------------------------------------------------')
+  console.log(data)
 
   csvWriter
     .writeRecords(data)
